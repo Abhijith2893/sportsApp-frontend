c289a36ec70ccbe0e11823aceca0bf273885711e
Abhijith Manohar <abhijithmanohar@Abhijiths-MacBook-Pro.local>
2020-10-05 20:52:25 -0500

    initial league table styling

--- src/components/LeagueTable/LeagueTable.jsx
+++ src/components/LeagueTable/LeagueTable.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import Loading from '../../components/Loading/Loading';
+import './LeagueTable.css';
 
 const LeagueTable = () => {
+    const [state,setState] = useState([]);
+    useEffect(() => {
+        const getData = async() => {
+            const apiKey = process.env.REACT_APP_ESPN_API_KEY;
+            const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/soccer/standings`,{headers:{'x-api-key':apiKey}});
+            setState(response.data);
+        }
+        getData();
+    },[]);
+    console.log(state)
     return(
         <div className="league-table">
-            "TEST"
+            {!state.length ? 
+                <Loading value="Getting EPL standings......" /> : 
+                <div className="league-table__table">
+                        {state.map((team,i) => {
+                            return(
+                                <div className="flex-table league-table__table__row" key={i}>
+                                    <div className="league-table__table__row__cell league-table__table__row__cell--name">
+                                        {team.name}
+                                    </div>
+                                    <div className="league-table__table__row__cell">
+                                        {team.played}
+                                    </div>
+                                    <div className="league-table__table__row__cell">
+                                        {team.win}
+                                    </div>
+                                    <div className="league-table__table__row__cell">
+                                        {team.loss}
+                                    </div>
+                                    <div className="league-table__table__row__cell">
+                                        {team.draw}
+                                    </div>
+                                    <div className="league-table__table__row__cell">
+                                        {team.points}
+                                    </div>
+                                </div>
+                            )
+                        })}
+                </div>
+            }
         </div>
     )
 };
